3a7960ff33c12a5e4d4092ed39078a1b6550d982
Nevil Jobanputra <111491420+Nevil844@users.noreply.github.com>
2024-05-06 08:10:56 +0530

    Update CoinFlip.sol

--- Games/CoinFlip.sol
+++ Games/CoinFlip.sol
@@ -2,47 +2,66 @@
 pragma solidity ^0.8.0;
 
 // Import the ProbabilityLibrary contract
-import "../ProbabilityLibrary.sol";
+import "./RandomWordLibrary.sol";
 
 contract CoinFlipGame {
 
+    RandomWordLibrary public randomWordLibrary;
+
+    bool public win; // Global variable to store win status
+
+    constructor(address _randomWordLibraryAddress) {
+        randomWordLibrary = RandomWordLibrary(_randomWordLibraryAddress); // Initialize with the RandomWordLibrary contract address
+    }
+
+    // Function to get the user's MetaMask address
+    function getUserAddress() public view returns (address) {
+        return msg.sender;
+    }
+
+
     // Define the possible outcomes of a coin flip
-    enum CoinOutcome { Heads, Tails }
+    enum CoinOutcome { Tails, Heads }
 
     // Define the probabilities for each outcome (assuming a fair coin)
     uint256 private constant fairCoinProbability = 50;
 
-    // Define the minimum and maximum bet amounts
-    uint256 private constant minBetAmount = 0.01 ether;
-    uint256 private constant maxBetAmount = 10 ether;
 
     // Event to log game outcomes
     event GameOutcome(address indexed player, uint256 betAmount, CoinOutcome outcome, bool win);
 
     // Function to place a bet on the outcome of a coin flip
-    function placeBet(CoinOutcome betOutcome) external payable {
-        // Validate the bet amount
-        require(msg.value >= minBetAmount && msg.value <= maxBetAmount, "Invalid bet amount");
+    function placeBet(CoinOutcome betOutcome) external payable returns (bool){
+
+        // Fetch the last request ID
+        uint256 requestId = randomWordLibrary.getLastRequestId();
 
         // Simulate flipping the coin
-        uint256 flipOutcomeValue = ProbabilityLibrary.simulateCoinFlip();
+        uint256 randomFlip = randomWordLibrary.fetchRandomWord(requestId) % 2; // Get dice roll from 1-6
         CoinOutcome flipOutcome;
 
-        if (flipOutcomeValue == 0) {
-            flipOutcome = CoinOutcome.Heads;
-        } else if (flipOutcomeValue == 1) {
+        if (randomFlip == 0) {
             flipOutcome = CoinOutcome.Tails;
+        } else if (randomFlip == 1) {
+            flipOutcome = CoinOutcome.Heads;
         } 
 
         // Check if the player wins
-        bool win = (flipOutcome == betOutcome);
+        win = (flipOutcome == betOutcome);
 
         // Emit the game outcome event
         emit GameOutcome(msg.sender, msg.value, flipOutcome, win);
 
         // Transfer winnings to the player if they win
         if (win) {
-            payable(msg.sender).transfer(msg.value * 2);
+            payable(msg.sender).transfer(0);
         }
+        return win;
     }
+
+    // Function to retrieve the latest value of win
+    function getWinStatus() external view returns (bool) {
+        return win;
+    }
+
 }
